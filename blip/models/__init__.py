4618edc42087d6191b286d0f54c42d7d61a2e26c
Quan-Sheng Zeng <zengqs@mail.nankai.edu.cn>
2023-05-09 11:46:27 +0800

    add setup and make blip installable

---
+++ blip/models/__init__.py
@@ -0,0 +1,6 @@
+from .blip_itm import BLIP_ITM
+from .blip import blip_decoder, blip_feature_extractor
+from .blip_vqa import blip_vqa
+from .blip_nlvr import blip_nlvr
+from .blip_pretrain import blip_pretrain
+from .blip_retrieval import blip_retrieval
